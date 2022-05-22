660e4f0dfd5b2e7b4dc28de068e303f2ddeb31c4
andrewyakovenko <andrew.yakovenko@gmail.com>
2022-05-23 02:04:27 +0300

    moved function out from parser

--- src/parser.ts
+++ src/parser.ts
@@ -7,6 +7,24 @@ export type YuvParserOptions = Y4MStreamOptions & {
     ffmpeg?: string
 }
 
+function parseFrame(data: Buffer, header: Y4MHeader, frameHeader: string, fn: number): YuvFrame {
+    if (!frameHeader.startsWith('FRAME')) {
+        throw 'Invalid frame header';
+    }
+    let luma = header.lumaSize();
+    let chroma = header.chromaSize();
+    return {
+        header: {
+            fn: fn
+        },
+        colorPlanes: {
+            y: new Gray(header.width, header.height, data.subarray(0, luma)),
+            u: new Gray(header.chromaWidth(), header.chromaHeight(), data.subarray(luma, luma + chroma)),
+            v: new Gray(header.chromaWidth(), header.chromaHeight(), data.subarray(luma + chroma))
+        }
+    }
+}
+
 export class YuvParser {
     private readonly options: YuvParserOptions;
 
@@ -14,23 +32,6 @@ export class YuvParser {
         this.options = options ?? {};
     }
 
-    private static parseFrame(data: Buffer, header: Y4MHeader, frameHeader: string, fn: number): YuvFrame {
-        if (!frameHeader.startsWith('FRAME')) {
-            throw 'Invalid frame header';
-        }
-        let luma = header.lumaSize();
-        let chroma = header.chromaSize();
-        return {
-            header: {
-                fn: fn
-            },
-            colorPlanes: {
-                y: new Gray(header.width, header.height, data.subarray(0, luma)),
-                u: new Gray(header.chromaWidth(), header.chromaHeight(), data.subarray(luma, luma + chroma)),
-                v: new Gray(header.chromaWidth(), header.chromaHeight(), data.subarray(luma + chroma))
-            }
-        }
-    }
 
     public read(path: string): Observable<YuvFrame> {
         return new Observable<YuvFrame>(subscriber => {
@@ -58,7 +59,7 @@ export class YuvParser {
                             }
                         } else {
                             const frameData = chunk.subarray(0, header.getFrameSize());
-                            subscriber.next(YuvParser.parseFrame(frameData, header, frameHeader, fn));
+                            subscriber.next(parseFrame(frameData, header, frameHeader, fn));
                             fn += 1;
                             frameHeader = chunk.subarray(header.getFrameSize()).toString('ascii');
                             if (this.options.verbose) {
@@ -70,7 +71,7 @@ export class YuvParser {
                 error: subscriber.error,
                 complete: () => {
                     if (buffer.length == header.getFrameSize()) {
-                        subscriber.next(YuvParser.parseFrame(buffer, header, frameHeader, fn));
+                        subscriber.next(parseFrame(buffer, header, frameHeader, fn));
                         subscriber.complete();
                     } else {
                         subscriber.error(`Incorrect size of last frame (${buffer.length}), expected ${header.getFrameSize()}`);
