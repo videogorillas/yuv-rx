65191b64693bbf3370520de7cb4f1813b076fd85
andrewyakovenko <andrew.yakovenko@gmail.com>
2023-04-10 19:55:01 +0300

    simplified more tests

--- src/tests/color.test.ts
+++ src/tests/color.test.ts
@@ -1,6 +1,6 @@
 import {path as ffmpegPath} from '@ffmpeg-installer/ffmpeg';
 import {YuvParser} from '../parser';
-import {map, toArray} from 'rxjs';
+import {lastValueFrom, map, toArray} from 'rxjs';
 import {toRGB, toRGBInterleaved} from '../color';
 
 function buffersDiff(b1: Buffer, b2: Buffer, ignoreThreshold?: number) {
@@ -21,83 +21,51 @@ function buffersDiff(b1: Buffer, b2: Buffer, ignoreThreshold?: number) {
     return diffs;
 }
 
-it('converts yuv to rgb', done => {
-    let tested = false;
-    new YuvParser({
+it('converts yuv to rgb', async () => {
+    const frames = await lastValueFrom(new YuvParser({
         ffmpeg: ffmpegPath
     }).read(`${__dirname}/resources/colors.mp4`).pipe(
         map(toRGB),
         toArray()
-    ).subscribe({
-        next: frames => {
-            expect(frames.length).toStrictEqual(6);
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[0].colorPlanes.r.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[0].colorPlanes.g.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[0].colorPlanes.b.data, 1)).toStrictEqual([]);
+    ));
+    expect(frames.length).toStrictEqual(6);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[0].colorPlanes.r.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[0].colorPlanes.g.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[0].colorPlanes.b.data, 1)).toStrictEqual([]);
 
-            expect(buffersDiff(Buffer.alloc(256, 0xff), frames[1].colorPlanes.r.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0xff), frames[1].colorPlanes.g.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0xff), frames[1].colorPlanes.b.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0xff), frames[1].colorPlanes.r.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0xff), frames[1].colorPlanes.g.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0xff), frames[1].colorPlanes.b.data, 1)).toStrictEqual([]);
 
-            expect(buffersDiff(Buffer.alloc(256, 0xff), frames[2].colorPlanes.r.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[2].colorPlanes.g.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[2].colorPlanes.b.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0xff), frames[2].colorPlanes.r.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[2].colorPlanes.g.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[2].colorPlanes.b.data, 1)).toStrictEqual([]);
 
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[3].colorPlanes.r.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0xff), frames[3].colorPlanes.g.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[3].colorPlanes.b.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[3].colorPlanes.r.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0xff), frames[3].colorPlanes.g.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[3].colorPlanes.b.data, 1)).toStrictEqual([]);
 
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[4].colorPlanes.r.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0), frames[4].colorPlanes.g.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0xff), frames[4].colorPlanes.b.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[4].colorPlanes.r.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0), frames[4].colorPlanes.g.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0xff), frames[4].colorPlanes.b.data, 1)).toStrictEqual([]);
 
-            expect(buffersDiff(Buffer.alloc(256, 0x42), frames[5].colorPlanes.r.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0x19), frames[5].colorPlanes.g.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256, 0x84), frames[5].colorPlanes.b.data, 1)).toStrictEqual([]);
-            tested = true;
-        },
-        error: err => {
-            console.error(err);
-            throw err;
-        },
-        complete: () => {
-            if (tested) {
-                done();
-            } else {
-                throw 'completed without emitting data';
-            }
-        }
-    });
+    expect(buffersDiff(Buffer.alloc(256, 0x42), frames[5].colorPlanes.r.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0x19), frames[5].colorPlanes.g.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256, 0x84), frames[5].colorPlanes.b.data, 1)).toStrictEqual([]);
 });
 
-it('converts yuv to interleaved rgb', done => {
-    let tested = false;
-    new YuvParser({
+it('converts yuv to interleaved rgb', async () => {
+    const frames = await lastValueFrom(new YuvParser({
         ffmpeg: ffmpegPath
     }).read(`${__dirname}/resources/colors.mp4`).pipe(
         map(toRGBInterleaved),
         toArray()
-    ).subscribe({
-        next: frames => {
-            expect(frames.length).toStrictEqual(6);
-            expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0, 0, 0])), frames[0].colorPlanes.rgb.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0xff, 0xff, 0xff])), frames[1].colorPlanes.rgb.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0xff, 0, 0])), frames[2].colorPlanes.rgb.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0, 0xff, 0])), frames[3].colorPlanes.rgb.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0, 0, 0xff])), frames[4].colorPlanes.rgb.data, 1)).toStrictEqual([]);
-            expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0x42, 0x19, 0x84])), frames[5].colorPlanes.rgb.data, 1)).toStrictEqual([]);
-            tested = true;
-        },
-        error: err => {
-            console.error(err);
-            throw err;
-        },
-        complete: () => {
-            if (tested) {
-                done();
-            } else {
-                throw 'completed without emitting data';
-            }
-        }
-    });
+    ));
+    expect(frames.length).toStrictEqual(6);
+    expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0, 0, 0])), frames[0].colorPlanes.rgb.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0xff, 0xff, 0xff])), frames[1].colorPlanes.rgb.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0xff, 0, 0])), frames[2].colorPlanes.rgb.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0, 0xff, 0])), frames[3].colorPlanes.rgb.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0, 0, 0xff])), frames[4].colorPlanes.rgb.data, 1)).toStrictEqual([]);
+    expect(buffersDiff(Buffer.alloc(256 * 3, Buffer.from([0x42, 0x19, 0x84])), frames[5].colorPlanes.rgb.data, 1)).toStrictEqual([]);
 });
