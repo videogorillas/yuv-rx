118ed537edc3068efc5d3778a164eab78e155432
Andrew Yakovenko <andrew.yakovenko@gmail.com>
2023-04-10 20:41:41 +0300

    ffmpeg log handler configuration (#19)
    * configurable ffmpeg log handler
    * simplified tests

--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -1,82 +1,35 @@
 import {path as ffmpegPath} from '@ffmpeg-installer/ffmpeg';
 import {YuvParser} from '../parser';
-import {toArray} from 'rxjs';
+import {lastValueFrom, toArray} from 'rxjs';
 
-it('parses video into frames', done => {
-    let tested = false;
-    new YuvParser({
+it('parses video into frames', async () => {
+    const framesRx = new YuvParser({
         ffmpeg: ffmpegPath,
         verbose: true
-    }).read(`${__dirname}/resources/colors.mp4`).pipe(toArray()).subscribe({
-        next: frames => {
-            expect(frames.length).toStrictEqual(6);
-            tested = true;
-        },
-        error: err => {
-            console.error(err);
-            throw err;
-        },
-        complete: () => {
-            if (tested) {
-                done();
-            } else {
-                throw 'completed without emitting data';
-            }
-        }
-    });
+    }).read(`${__dirname}/resources/colors.mp4`);
+    const frames = await lastValueFrom(framesRx.pipe(toArray()));
+    expect(frames.length).toStrictEqual(6);
 });
 
-it('handles empty stream correctly', done => {
-    let tested = false;
-    new YuvParser({
+it('handles empty stream correctly', async () => {
+    const frames = await lastValueFrom(new YuvParser({
         ffmpeg: ffmpegPath,
         verbose: true
     }).read(`${__dirname}/resources/colors.mp4`, {
         seekSeconds: 42
-    }).pipe(toArray()).subscribe({
-        next: frames => {
-            expect(frames.length).toStrictEqual(0);
-            tested = true;
-        },
-        error: err => {
-            console.error(err);
-            throw err;
-        },
-        complete: () => {
-            if (tested) {
-                done();
-            } else {
-                throw 'completed without emitting data';
-            }
-        }
-    });
+    }).pipe(toArray()));
+    expect(frames.length).toStrictEqual(0);
 });
 
-it('parses MPEG-DASH into frames', done => {
-    let tested = false;
+it('parses MPEG-DASH into frames', async () => {
     const basename = `${__dirname}/resources/stream`;
     const input = `concat:${basename}-2.0.m4s|${basename}-2.2.m4s`;
-    new YuvParser({
+    const frames = await lastValueFrom(new YuvParser({
         ffmpeg: ffmpegPath,
         verbose: true
     }).read(input, {
         seekSeconds: 1,
         vframes: 3
-    }).pipe(toArray()).subscribe({
-        next: frames => {
-            expect(frames.length).toStrictEqual(3);
-            tested = true;
-        },
-        error: err => {
-            console.error(err);
-            throw err;
-        },
-        complete: () => {
-            if (tested) {
-                done();
-            } else {
-                throw 'completed without emitting data';
-            }
-        }
-    });
+    }).pipe(toArray()));
+    expect(frames.length).toStrictEqual(3);
 });
