59bbd45460156cd8abd13124bd541edf0a3e7615
Andrew Yakovenko <andrew.yakovenko@gmail.com>
2023-04-18 16:33:02 +0300

    Added bilinear scaling for YUV frames (#22)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {ColorPlane, Rectangle, Dimension, Frame, FrameHeader, YuvFrame, Rational, InterleavedRgbFrame, InterleavedRgbaFrame, RgbFrame, Gray} from './frame';
 export {YuvParser, YuvParserOptions} from './parser';
 export {toRGB, toRGBInterleaved, toRGBAInterleaved} from './color';
+export {scaleBilinear} from './operators';
